e647061785d58b39329c9bec62a188e1ae115d31
LAPTOP-JAD1OL75\User <dream7652210@gmail.com>
2018-03-22 15:45:03 +0800

    產生一個亂數

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            Console.WriteLine(rand.Next(0, 10000));
+            Console.Read();
+        }
+    }
+}
