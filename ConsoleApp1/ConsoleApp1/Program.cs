9ce670f2de2f47b763a11d7dfe39ebb903c54aae
LAPTOP-JAD1OL75\User <dream7652210@gmail.com>
2018-03-22 16:02:20 +0800

    在螢幕上顯示6個亂數

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,9 +10,20 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            // 產生亂數物件
             Random rand = new Random();
-            Console.WriteLine(rand.Next(0, 10000));
-            Console.Read();
+
+            // 設定6個變數
+            int r1 = rand.Next(1, 42);
+            int r2 = rand.Next(1, 42);
+            int r3 = rand.Next(1, 42);
+            int r4 = rand.Next(1, 42);
+            int r5 = rand.Next(1, 42);
+            int r6 = rand.Next(1, 42);
+
+            // 顯示
+            Console.WriteLine("{0:00} {1:00} {2:00} {3:00} {4:00} {5:00}" , r1 ,r2 , r3 , r4, r5 ,r6 );
+            Console.ReadLine();
         }
     }
 }
